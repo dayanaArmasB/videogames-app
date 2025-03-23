c4e32a4cc3a2c7e9dbb8a7cafb4c47256dfd50fb
Dayana <dyarmasb@hotmail.com>
2025-03-23 00:49:51 -0500

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    backendUrl: 'consumeapigames-production.up.railway.app' // URL de tu backend en desarrollo
+    backendUrl: 'consumeapigames-production.up.railway.app/api' // URL de tu backend en desarrollo
   };
   
 // 

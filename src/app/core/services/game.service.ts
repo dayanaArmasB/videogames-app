4896cf038a38910f1f71d943af8e43a0255454d0
Dayana <dyarmasb@hotmail.com>
2025-03-23 00:47:04 -0500

    update

--- src/app/core/services/game.service.ts
+++ src/app/core/services/game.service.ts
@@ -3,17 +3,17 @@ import { HttpClient } from '@angular/common/http';
 import { map, Observable } from 'rxjs';
 import { Game } from '../models/game.model';
 import { environment } from '../../../environments/environment';
-
+const base_url = environment.backendUrl;
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
-  private readonly backendUrl = environment.backendUrl;  
+ 
 
   constructor(private readonly http: HttpClient) {}
  
   getGames(page: number, genres?: string): Observable<any> {
-    let url = `${this.backendUrl}/games?page=${page}`;
+    let url = `${base_url}/games?page=${page}`;
     
     if (genres) {
       url += `&genres=${genres}`;
@@ -25,13 +25,13 @@ export class GameService {
 
   // Buscar juegos desde el backend
   searchGames(query: string): Observable<Game[]> {
-    const url = `http://localhost:8080/api/search?query=${encodeURIComponent(query)}`;
+    const url = `${base_url}/api/search?query=${encodeURIComponent(query)}`;
     return this.http.get<Game[]>(url);
   }
   
   // Obtener detalles de un juego específico desde el backend
   getGameDetails(id: number): Observable<Game> {
-    return this.http.get<Game>(`${this.backendUrl}/${id}`);
+    return this.http.get<Game>(`${base_url}/${id}`);
   }
 }
 

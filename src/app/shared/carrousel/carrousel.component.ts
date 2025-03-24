a1be977f78603ce17942955d795d0a8db8c60bf2
Dayana <dyarmasb@hotmail.com>
2025-03-23 19:58:22 -0500

    creacion de componente carrusel

---
+++ src/app/shared/carrousel/carrousel.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+// import function to register Swiper custom elements
+import { register } from 'swiper/element/bundle';
+// register Swiper custom elements
+@Component({
+  selector: 'app-carrousel',
+  standalone: true,
+  imports: [],
+  templateUrl: './carrousel.component.html',
+  styleUrl: './carrousel.component.css'
+})
+export class CarrouselComponent {
+
+}
